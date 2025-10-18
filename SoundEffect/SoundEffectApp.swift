4c2f7a7d61c8208772108aa51c49cd095263063f
Ali Moeeny <alimoeeny@gmail.com>
2025-10-18 11:26:07 -0400

    Initial Commit

---
+++ SoundEffect/SoundEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundEffectApp.swift
+//  SoundEffect
+//
+//  Created by Ali Moeeny on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
